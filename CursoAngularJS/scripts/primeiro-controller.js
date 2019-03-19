e00fca342ba596b3e7fb1f5f65084fd175ddf902
IltonBJSilva <xlsonplay@gmail.com>
2019-03-19 14:59:11 -0300

    Adiciona um array que implementa nome de alunos

--- CursoAngularJS/scripts/primeiro-controller.js
+++ CursoAngularJS/scripts/primeiro-controller.js
@@ -2,4 +2,7 @@ angular.module('aplicacao').controller('PrimeiroController',function($scope) {
   $scope.nome = 'Ilton Batista';
   var nome = "Ola ola ola";
   $scope.trab = true;
+
+  $scope.alunos = ['Ilton', 'Elian', 'Fabio','João','Ezequiel']
+
 });
