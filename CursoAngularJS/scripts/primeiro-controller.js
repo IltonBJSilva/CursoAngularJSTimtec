033b73076d159b6d21a84207a2b29fc3339bcecc
IltonBJSilva <xlsonplay@gmail.com>
2019-03-20 15:39:59 -0300

    Adiciona campo idade

--- CursoAngularJS/scripts/primeiro-controller.js
+++ CursoAngularJS/scripts/primeiro-controller.js
@@ -1,9 +1,15 @@
 angular.module('aplicacao').controller('PrimeiroController', function($scope){
 	$scope.nome = 'Ilton Batista';
 	var nome = "Aleatorio";
+
 	$scope.iniciado = true;
-	$scope.alunos = ['Elian', 'Ilton', 'João', 'Ezequiel', 'Fabio'];
-	$scope.hoje = new Date();
+
+	$scope.alunos = [{'nome': 'Elian', 'idade': 9},
+	                 {'nome': 'Ilton', 'idade': 11},
+	                 {'nome': 'João', 'idade': 11},
+	                 {'nome': 'Francisco', 'idade': 14},
+	                 {'nome': 'Sara', 'idade': 10},
+	                 {'nome': 'Andressa', 'idade': 12}];	$scope.hoje = new Date();
 
 	$scope.finalizar = function(){
 		$scope.iniciado = false;
@@ -13,8 +19,15 @@ angular.module('aplicacao').controller('PrimeiroController', function($scope){
 		$scope.iniciado = true;
 	};
 
-	$scope.submeter = function(){
-		$scope.alunos.push($scope.nome_aluno);
-	}
+	$scope.submeter = function() {
+		if($scope.form_1.$valid) {
+			novo_aluno = {};
+			novo_aluno['nome'] = $scope.nome_aluno;
+			novo_aluno['idade'] = parseInt($scope.idade_aluno);
+			$scope.alunos.push(novo_aluno);
+		} else {
+			alert("Preencha o formulário corretamente");
+		}
+	};
 
 });
