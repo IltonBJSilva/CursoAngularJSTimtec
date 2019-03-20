8a8f9696f47f7df18e9c8d43c5d0d5673924e0a5
IltonBJSilva <xlsonplay@gmail.com>
2019-03-20 14:54:58 -0300

    Layout alterado

--- CursoAngularJS/scripts/primeiro-controller.js
+++ CursoAngularJS/scripts/primeiro-controller.js
@@ -1,21 +1,16 @@
-angular.module('aplicacao').controller('PrimeiroController',function($scope) {
-  $scope.nome = 'Ilton Batista';
-  var nome = "Ola ola ola";
-  $scope.iniciado = true;
-  $scope.alunos = ['Ilton', 'Elian', 'Fabio','João','Ezequiel']
-  $scope.hoje = new Date();
-
-  $scope.finalizar = function(){
-    $scope.iniciado = false;
-  };
-  $scope.iniciar = function(){
-    $scope.iniciado = true;
-  }
-  $scope.submeter = function(){
-    if($scope.form_1.$valid){
-      $scope.alunos.push($scope.nome_aluno);
-    }else{
-      alert('Preencha o formulario corretamente');
-    }
-   }
+angular.module('aplicacao').controller('PrimeiroController', function($scope){
+	$scope.nome = 'Gabriel Pinheiro';
+	var nome = "Lá Lá Lá";
+	
+	$scope.iniciado = true;
+	
+	$scope.alunos = ['Aluno 1', 'Aluno 2', 'Aluno 3', 'Aluno 4'];
+	
+	$scope.finalizar = function(){
+		$scope.iniciado = false;
+	};
+	
+	$scope.iniciar = function() {
+		$scope.iniciado = true;
+	};
 });
